df1133aab81a5d2f0ce0712f61878fe16d82c6ad
Adrian-Bogdan Leancă <leancabogdan@outlook.com>
2020-01-11 20:11:15 +0200

    Added skeleton for login page

---
+++ RecruitBee/RecruitBee/controller/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  RecruitBee
+//
+//  Created by Adrian-Bogdan Leanca on 11/01/2020.
+//  Copyright © 2020 Adrian-Bogdan Leanca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .orange
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+        view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
+        
+        setupInputsContainerView()
+        setupLoginRegisterButton()
+    }
+    
+    func setupInputsContainerView() {
+        // need x, y, height, width constraints
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    func setupLoginRegisterButton() {
+        // need x, y, height, width constraints
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+}
+
