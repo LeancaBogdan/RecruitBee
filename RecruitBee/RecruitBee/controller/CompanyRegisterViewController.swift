95f97ae3e12379332aaa174c1a4080189f1b7ca1
Alex Popsor <popsor.balex@yahoo.com>
2020-01-12 15:59:37 +0200

    Student and Company screens

--- RecruitBee/RecruitBee/controller/CompanyRegisterViewController.swift
+++ RecruitBee/RecruitBee/controller/CompanyRegisterViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class CompanyRegisterViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UIStackView!
+    @IBOutlet weak var lastNameTextField: UIStackView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
 
+    @IBAction func registerButtonPressed(_ sender: Any) {
+    }
 }
