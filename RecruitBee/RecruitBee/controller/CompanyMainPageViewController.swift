1f2b2557ae6353a2e55869b3c4128a60bd102b34
Alex Popsor <popsor.balex@yahoo.com>
2020-01-13 20:58:40 +0200

    small changes

--- RecruitBee/RecruitBee/controller/CompanyMainPageViewController.swift
+++ RecruitBee/RecruitBee/controller/CompanyMainPageViewController.swift
@@ -14,11 +14,41 @@ class CompanyMainPageViewController: UIViewController {
     var ref = Database.database().reference()
     let user = Auth.auth().currentUser
     
+    @IBOutlet weak var signOutButton: UIBarButtonItem!
+    @IBOutlet weak var addJobOfferButton: UIBarButtonItem!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        self.ref.child("companies").child(user!.uid).observeSingleEvent(of: .value) { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+                
+            if value == nil {
+                return
+            }
+            else {
+                self.navigationBar.topItem?.title = value!["companyName"] as? String
+            }
+            
+        }
     }
 
 
+    @IBAction func addJobOfferButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "toJobCreate", sender: self)
+    }
+    
+    @IBAction func signOutButtonPressed(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+            return
+        }
+        
+        UserDefaults.standard.setIsLoggedIn(value: false)
+        performSegue(withIdentifier: "logoutCompany", sender: self)
+        
+    }
 }
